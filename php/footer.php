2afbc3e23e29652b1f4fe526513da92ba0d567a9
AngeloPatrick <angelopatrickriosnolasco@gmail.com>
2021-11-24 11:36:22 -0500

    Modificación de footer, nuevo footer con los logos y nuevo diseño

--- php/footer.php
+++ php/footer.php
@@ -1,51 +1,56 @@
-<!-- footer de la pagina -->
-<footer class="footer">
-        <div class="contenedor-seccion">
-            <p>Enterate primero de todas nuestras ofertas</p>
-            <div class="input-footer-seccion-1">
-                <input type="text" placeholder="Correo electrónico">
-                <button><i class="fas fa-angle-double-right"></i></button>
-            </div>
-            <div class="contenedor-redes-sociales-footer">
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
-            </div>
+    <!-- footer de la pagina -->
+    <footer class="container-footer">
+    <div class="container-seccion">
+        <p>Entérate de nuestras ofertas</p>
+        <div class="container-input-email-footer">
+            <input type="email" placeholder="Correo electrónico">
+            <button><i class="fas fa-angle-double-right"></i></button>
         </div>
-        <div class="contenedor-seccion">
-            <p>Información para el cliente</p>
-            <div>
-                <ul>
-                    <li><a href="">Contáctenos</a></li>
-                    <li><a href="">Nuestras tiendas</a></li>
-                    <li><a href="">Libro de reclamaciones</a></li>
-                    <li><a href="">Cambio y devoluciones</a></li>
-                </ul>
-            </div>
+        <div class="container-redes-sociales-footer">
+            <a href="">
+                <i class="fab fa-facebook"></i>
+            </a>
+            <a href="">
+                <i class="fab fa-whatsapp"></i>
+            </a>
+            <a href="">
+                <i class="fab fa-instagram"></i>
+            </a>
         </div>
-        <div class="contenedor-seccion">
-            <p>Gestión de cuenta</p>
-            <div>
-                <ul>
-                    <li><a href="">Mi cuenta</a></li>
-                    <li><a href="">Registrate</a></li>
-                    <li><a href="">Actualizar datos</a></li>
-                    <li><a href="">Cambiar contraseña</a></li>
-                </ul>
-            </div>
+    </div>
+    <div class="container-seccion">
+        <p>Información para el cliente</p>
+        <div class="container-ul-seccion">
+            <ul>
+                <li><a href="">Contáctenos</a></li>
+                <li><a href="">Nuestras tiendas</a></li>
+                <li><a href="">Libro de reclamaciones</a></li>
+                <li><a href="">Cambio y devoluciones</a></li>
+            </ul>
         </div>
-        <div class="contenedor-seccion">
-            <p>Medios de pago</p>
-            <div class="medios-pago">
-
-            </div>
+    </div>
+    <div class="container-seccion">
+        <p>Gestión de cuenta</p>
+        <div class="container-ul-seccion">
+            <ul>
+                <li><a href="">Mi cuenta</a></li>
+                <li><a href="">Registrate</a></li>
+                <li><a href="">Actualizar datos</a></li>
+                <li><a href="">Cambiar contraseña</a></li>
+            </ul>
         </div>
+    </div>
+    <div class="container-seccion">
+        <p>Medios de pago</p>
+        <div class="medios-pago-logo">
+            <img src="<?php echo $link_base_root?>/imagenes_banner/imagenes_pagos/unnamed.png" alt="Logo de método de pago">
+            <img src="<?php echo $link_base_root?>/imagenes_banner/imagenes_pagos/bcp_logo.jpg" alt="Logo de método de pago">
+        </div>
+    </div>
     </footer>
     <div class="contenedor-banner-footer">
         <div class="contenedor-footer-img">
-            <img src='http://localhost:8080/PROYECTO-OFICIAL-FINAL\imagenes\footer-logo.png' alt="">
+            <img src='<?php echo $link_base_root?>/imagenes_banner/footer-logo.png' alt="">
         </div>
     </div>
 </body>
