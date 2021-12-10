e69fd531dfefa74c751d74b47831b8f8d5bdb8da
AngeloPatrick <angelopatrickriosnolasco@gmail.com>
2021-12-09 23:51:18 -0500

    Muestra las imagenes del banner dependiendo de los datos que se encuentren en la base de datos

--- javascript/script-slider.js
+++ javascript/script-slider.js
@@ -2,8 +2,8 @@ var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
--- slider/slider_imagenes.php
+++ slider/slider_imagenes.php
@@ -1,12 +1,27 @@
+<!-- ya existe conexion a la base de datos en el "header" -->
+<!-- nesitamos la direccion de la imagen -->
+
+<?php
+  $consulta_slider = "SELECT direccion_imagen FROM banner_img";
+  $imagenes = $conn->query($consulta_slider);
+?>
 <div class="slideshow-container">
-<div class="mySlides fade">
-  <img src="<?php echo $link_base_root?>/imagenes_banner/laptops_4.jpg">
-</div>
-<div class="mySlides fade">
-  <img src="<?php echo $link_base_root?>/imagenes_banner/h2-miray_4.jpg">
-</div>
-<div class="mySlides fade">
-  <img src="<?php echo $link_base_root?>/imagenes_banner/h1-miray_9.jpg">
-</div>
+  <div class="mySlides fade">
+    <img src="<?php echo $link_base_root?>/imagenes_banner/laptops_4.jpg">
+  </div>
+  <div class="mySlides fade">
+    <img src="<?php echo $link_base_root?>/imagenes_banner/h2-miray_4.jpg">
+  </div>
+  <div class="mySlides fade">
+    <img src="<?php echo $link_base_root?>/imagenes_banner/h1-miray_9.jpg">
+  </div>
+  <?php
+    while($row_imagen = $imagenes->fetch_assoc()){?>
+    <div class="mySlides fade">
+      <img src="<?php echo $row_imagen["direccion_imagen"]?>">
+    </div>
+  <?php
+    }
+  ?>
 </div>
 <script src="<?php echo $link_base_root?>/javascript/script-slider.js"></script>
